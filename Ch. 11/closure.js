9b59672496507d7949b5f72f24455ef81affe554
awp1121 <awpollon@gmail.com>
2014-09-21 23:29:13 -0400

    implemetned with closure

--- Ch. 11/closure.js
+++ Ch. 11/closure.js
@@ -1,21 +1,15 @@
 /**
  * @author Aaron Pollon
  */
-var count = 0;
-
-
-
-function buttonClicked(){
-	count++;
-	var output = document.getElementById("message");
-	output.innerHTML = "Current Count: " + count;
-}
-
 
 window.onload = function(){
-var button = document.getElementById("clickme");
-
-button.onclick = buttonClicked;
+	var count = 0;
+	var output = document.getElementById("message");
+	var button = document.getElementById("clickme");
+			
+	button.onclick = function (){
+		count++;
+		output.innerHTML = "Current Count: " + count;
+	};		
 };
-
 
